e337918d3ad1b2f131418bb90a866e4f2df2d9ee
KayO-GH <kayoghonline@gmail.com>
2017-05-05 18:07:18 +0000

    Home section fixed with blogspot advertisement scheme

--- app/src/main/java/com/roomiegh/roomie/tabs/TabHome.java
+++ app/src/main/java/com/roomiegh/roomie/tabs/TabHome.java
@@ -1,20 +1,31 @@
 package com.roomiegh.roomie.tabs;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 import com.roomiegh.roomie.R;
+import com.roomiegh.roomie.util.ReadRSS;
 
 /**
  * Created by Kwadwo Agyapon-Ntra on 06/10/2015.
  */
 public class TabHome extends Fragment{
-    private WebView wvHome;
+    private static final String HOME_TAG = "HomeAdsView";
+    //private WebView wvHome;
+    RecyclerView recyclerView;
+    ScrollView svConnectionError;
+    Context ctx;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,13 +33,39 @@ public class TabHome extends Fragment{
 
         init(view);
 
-        wvHome.setWebViewClient(new WebViewClient());
-        wvHome.loadUrl("http://www.roomiegh.com");
+        /*wvHome.setWebViewClient(new WebViewClient());
+        wvHome.loadUrl("http://www.roomiegh.com");*/
+        if (!connectedToNetwork()){
+            //not connected to mobile data or WIFI
+            Toast.makeText(getActivity(), R.string.check_connection, Toast.LENGTH_LONG).show();
+            //fab.setVisibility(View.INVISIBLE);
+        }
+        ReadRSS readRSS =   new ReadRSS(getActivity(), recyclerView, svConnectionError);
+        Log.d(HOME_TAG, "onCreateView: about to call ReadRSS");
+        readRSS.execute();
 
         return view;
     }
 
     private void init(View view) {
-        wvHome = (WebView) view.findViewById(R.id.wvHome);
+        //wvHome = (WebView) view.findViewById(R.id.wvHome);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        svConnectionError = (ScrollView) view.findViewById(R.id.svConnectionError);
+
+    }
+
+    private boolean connectedToNetwork() {
+        /*//The code below will not work properly on the main thread
+        InetAddress ipAddr = null;
+        try{
+            ipAddr = InetAddress.getByName("google.com");
+            return !ipAddr.toString().equals("");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, ipAddr+"", Toast.LENGTH_LONG).show();
+            return false;
+        }*/
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(getActivity().CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo()!=null;
     }
 }
